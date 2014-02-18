b905c38e1ed306ea85b997649efcab9d685106cd
Bartuz <filip@bartuzi.me>
2014-02-18 10:03:55 +0100

    algorythm is working

--- zigzag_cipher.cpp
+++ zigzag_cipher.cpp
@@ -2,23 +2,22 @@
 #include <string>
 using namespace std;
 int main(int argc, char *argv[]) {
-	string score, text = "kryptoanaliza";
-	int columns,n;
+	string score, text = "kot";
+	int columns,chars_per_line;
 //	cin >> columns;
-	columns = 4;
-	n = (text.length() / columns);
-	char cipher[columns][n];
+	columns = 2;
+	chars_per_line = (text.length() / columns) + 1;
+	char cipher[columns][chars_per_line];
 	int k = 0;
 	for (int i = 0; i < columns; i++) {
-		for (int j = 0; k < n; j++) {
+		for (int j = 0; j < chars_per_line; j++) {
 			cipher[i][j] = text[k];
 			k++;
-			if (k > text.length()) break;
 		}
 	}
-	for (int i=0; i< n; i++) {
+	for (int i = 0; i < chars_per_line; i++) {
 		for (int j = 0; j < columns; j++) {
-			score += cipher[i][j];
+			score += cipher[j][i];
 		}
 	}
 	cout << score;
